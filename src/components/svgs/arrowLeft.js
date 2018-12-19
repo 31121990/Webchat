6594fb192c4b00f368258f7db890461732e73ad2
31121990 <30981767+31121990@users.noreply.github.com>
2018-12-19 16:55:52 +0530

    Add files via upload

--- src/components/svgs/arrowLeft.js
+++ src/components/svgs/arrowLeft.js
@@ -3,7 +3,7 @@ import React from 'react'
 const arrowLeft = ({ className }) => {
   return (
     <svg width={16} height={16} viewBox="0 0 512 512" className={className}>
-      <path d="M320 128L192 256l128 128z" fill="grey" />
+      <path d="M320 128L192 256l128 128z" fill="red" />
     </svg>
   )
 }
