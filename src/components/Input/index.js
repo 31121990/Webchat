9121f43a30433dada299b650b5e4a919032c74d8
31121990 <30981767+31121990@users.noreply.github.com>
2018-12-19 15:47:16 +0530

    Send button commented

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -218,11 +218,11 @@ class Input extends Component {
           rows={1}
         />
         
-        <SendButton
+       /* <SendButton
           preferences={preferences}
           sendMessage={this.sendMessage}
           value={value}
-        />
+        />*/
         
         {showLimitCharacter && (
           <div className="characterLimit">{characterLimit - value.length}</div>
