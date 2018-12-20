27a8ac78ce4606b8cfb372af8cd40cc2942036bb
31121990 <30981767+31121990@users.noreply.github.com>
2018-12-20 15:39:39 +0530

    Add files via upload

--- webchat.js
+++ webchat.js
@@ -61,7 +61,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "54464a6210e2447e359c"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "57a4381983ffb0d0b123"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -56585,7 +56585,9 @@ var Input = function (_Component) {
           },
           rows: 1
         }),
+        '/*Vadi ',
         _react2.default.createElement(_SendButton2.default, { preferences: preferences, sendMessage: this.sendMessage, value: value }),
+        '*/',
         _react2.default.createElement(_SendButton2.default, { preferences: preferences, sendMessage: this.sendMessage, value: value }),
         showLimitCharacter && _react2.default.createElement(
           'div',
