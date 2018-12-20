123a38db9069e82b6d0fc1cfdc1b9e21a8f73aea
31121990 <30981767+31121990@users.noreply.github.com>
2018-12-20 14:42:09 +0530

    revert to old

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -218,7 +218,7 @@ class Input extends Component {
           rows={1}
         />
 
-        /*Vadi<SendButton preferences={preferences} sendMessage={this.sendMessage} value={value} />*/
+        <SendButton preferences={preferences} sendMessage={this.sendMessage} value={value} />
 
         {showLimitCharacter && (
           <div className="characterLimit">{characterLimit - value.length}</div>
