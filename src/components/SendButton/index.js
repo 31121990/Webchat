089349811a99fd0bffb8d0ce5332c7aede11ef3c
31121990 <30981767+31121990@users.noreply.github.com>
2018-12-20 12:35:39 +0530

    Add files via upload

--- src/components/SendButton/index.js
+++ src/components/SendButton/index.js
@@ -5,8 +5,8 @@ import './style.scss'
 
 const SendButton = ({ sendMessage, preferences, value }) => (
   <div className="RecastSendButtonContainer">
-    <div onClick={sendMessage} disabled={!value}>
-      <svg
+    <div onClick={sendMessage} disabled={!value} class="circle-wrap circle-wrap--third">
+     /* <svg
         style={{
           width: 33,
           fill: value ? preferences.accentColor : preferences.botMessageColor,
@@ -14,7 +14,13 @@ const SendButton = ({ sendMessage, preferences, value }) => (
         viewBox="0 0 512 512"
       >
         <path d="M85 277.375h259.704L225.002 397.077 256 427l171-171L256 85l-29.922 29.924 118.626 119.701H85v42.75z" />
-      </svg>
+      </svg>*/
+	  
+	   <svg name="play-button" viewBox="0 0 1000 1000">
+    <use class="play-button-circle" xlink:href="#play-button-circle"></use>
+    <use class="play-button-triangle" xlink:href="#play-button-triangle"></use>
+  </svg>
+  
     </div>
   </div>
 )
