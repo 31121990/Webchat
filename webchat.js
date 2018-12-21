0cf7f830639cfd5ba3b67ff46f1da6ef979b71fe
31121990 <30981767+31121990@users.noreply.github.com>
2018-12-21 15:10:17 +0530

    Add files via upload

--- webchat.js
+++ webchat.js
@@ -61,7 +61,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "6b15e0e485e6e2bb8375"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "9dce10426ef97321b4d4"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -56668,14 +56668,14 @@ var MicButton = function MicButton(_ref) {
                 onClick: micMessage,
                 disabled: !value
             },
-            _react2.default.createElement('img', { src: '../robot_icon.PNG' }),
+            _react2.default.createElement('img', { src: './svgs/robot_icon.PNG' }),
             _react2.default.createElement(
                 'svg',
                 { height: '30', width: '200' },
                 _react2.default.createElement(
                     'text',
                     { x: '0', y: '15', fill: 'red' },
-                    'Micbtn'
+                    'Micbtn1'
                 )
             )
         )
