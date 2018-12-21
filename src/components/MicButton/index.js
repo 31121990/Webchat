d7ab647d63b90ee39852adb02c95c32721e68275
31121990 <30981767+31121990@users.noreply.github.com>
2018-12-21 11:42:04 +0530

    Add files via upload

--- src/components/MicButton/index.js
+++ src/components/MicButton/index.js
@@ -12,15 +12,7 @@ const MicButton = ({sendMessage, preferences, value}) => (
             onClick={sendMessage}
             disabled={!value}
         >
-            <svg
-                style={{
-                    width: 43,
-                    fill: value,
-                }}
-                viewBox="0 0 512 512"
-            >
-                  <path d="M85 277.375h259.704L225.002 397.077 256 427l171-171L256 85l-29.922 29.924 118.626 119.701H85v42.75z"></path>
-            </svg>
+            <Text text="MicBtn" />
 			
         </div>
 		
